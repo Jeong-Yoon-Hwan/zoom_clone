916c452e2720c64e39d2b7836033147313b74b90
Jeong-Yoon-Hwan <wjddbsghks5742@gmail.com>
2023-04-19 02:34:59 +0900

    pug 추가

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 import express from "express";
+import { render } from "pug";
 
 const app  = express();
-console.log("hello");
-
-app.listen(3000);
+app.set("view engine", "pug");
+app.set("views",__dirname + "/  views")
+app.use("/public",express.static(__dirname + "/public"))
+app.get("/", (req,res) => res.render("home"));
+const handleListen = () => console.log(`Listening on http:localhost:3000`);
+app.listen(3000,handleListen);   
