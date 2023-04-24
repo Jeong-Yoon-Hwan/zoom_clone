2e05a6549058bab253c0565cd32c21f95d376a54
Jeong-Yoon-Hwan <wjddbsghks5742@gmail.com>
2023-04-25 03:32:44 +0900

    socket send

--- src/server.js
+++ src/server.js
@@ -14,4 +14,13 @@ const handleListen = () => console.log(`Listening on http:localhost:3000`);
 const server = http.createServer(app);
 
 const wss = new WebSocket.Server({server});
-server.listen(3000, handleListen)
+
+wss.on('connection', (socket)=>{
+    console.log("Conneted to Browser✔");
+    socket.on('close',()=> console.log("Disconnected from the Browser ❌"))
+    socket.on('message',(message)=>{
+        console.log(message.toString());
+    })
+    socket.send("hello!!!");
+})
+server.listen(3000)
